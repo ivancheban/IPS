678a7af073d2e6403acb45d0bba8fa14fd269fda
eduar <eduardvitko>
2022-06-28 15:46:45 +0300

    setting AdminAuthenticationFilter

--- src/main/java/com/filters/AdminAuthenticationFilter.java
+++ src/main/java/com/filters/AdminAuthenticationFilter.java
@@ -1,56 +1,56 @@
-//package com.filters;
-//
-//import javax.servlet.*;
-//import javax.servlet.annotation.WebFilter;
-//import javax.servlet.http.HttpServletRequest;
-//import javax.servlet.http.HttpSession;
-//import java.io.IOException;
-//
-//@WebFilter("/user-list.jsp")
-//public class AdminAuthenticationFilter implements Filter {
-//
-//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-//            throws IOException, ServletException {
-//
-//        HttpServletRequest httpRequest = (HttpServletRequest) request;
-//        HttpSession session = httpRequest.getSession(false);
-//
-//        boolean isLoggedIn = (session != null && session.getAttribute("adminUser") != null);
-//
-//        String loginURI = httpRequest.getContextPath() + "/admin/login";
-//
-//        boolean isLoginRequest = httpRequest.getRequestURI().equals(loginURI);
-//
-//        boolean isLoginPage = httpRequest.getRequestURI().endsWith("login.jsp");
-//
-//        if (isLoggedIn && (isLoginRequest || isLoginPage)) {
-//            // the admin is already logged in and he's trying to login again
-//            // then forwards to the admin's homepage
-//            RequestDispatcher dispatcher = request.getRequestDispatcher("/user-list.jsp");
-//            dispatcher.forward(request, response);
-//
-//        } else if (isLoggedIn || isLoginRequest) {
-//            // continues the filter chain
-//            // allows the request to reach the destination
-//            chain.doFilter(request, response);
-//
-//        } else {
-//            // the admin is not logged in, so authentication is required
-//            // forwards to the Login page
-//            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
-//            dispatcher.forward(request, response);
-//
-//        }
-//
-//    }
-//
-//    public void AdminLoginFilter() {
-//    }
-//
-//    public void destroy() {
-//    }
-//
-//    public void init(FilterConfig fConfig) throws ServletException {
-//    }
-//
-//}
+package com.filters;
+
+import com.auth.JWTService;
+import com.auth.Payload;
+import com.model.Role;
+import com.service.UserService;
+import com.service.UserServiceImpl;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/user-list.jsp","/add/subscription","/add/tariff/service","/delete/subscription","/subscriptions","/update/subscription","/update/form",
+        "/addTariff","/delete/tariff","/tariffs","/user/delete","/user/find","/user.do","/user/update"})
+public class AdminAuthenticationFilter implements Filter {
+    private static Logger logger = LogManager.getLogger(AdminAuthenticationFilter.class);
+    private static final int STATUS_CODE_UNAUTHORIZED = 401;
+     @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+        HttpSession session = httpRequest.getSession();
+        JWTService jwtService = new JWTService();
+        UserService userService = new UserServiceImpl();
+
+        try {
+            String jwt = (String) session.getAttribute("token");
+
+            if (jwt != null && !jwt.isEmpty()) {
+                Payload payload = jwtService.verifyToken(jwt);
+                String phone = payload.getClaims().get("phone");
+                Role role = userService.findByPhoneNumber(phone).getRole();
+
+                if (role.equals(Role.ADMIN)){
+                    filterChain.doFilter(servletRequest, servletResponse);
+                }  else {
+                    RequestDispatcher requestDispatcher = httpRequest.getRequestDispatcher("/error.jsp");
+                    requestDispatcher.forward(servletRequest, servletResponse);
+                }
+
+            } else {
+                httpResponse.sendRedirect("/login.jsp");
+            }
+
+
+        } catch (final Exception e) {
+            logger.debug("Failed logging in with security token");
+            httpResponse.sendRedirect("/login.jsp");
+        }
+    }
+}

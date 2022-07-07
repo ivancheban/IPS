2a60216e95b07e0e4b7385fd1e865fb9acaaab1b
eduar <eduardvitko>
2022-07-07 22:49:21 +0300

    junit testing USER DAO

--- src/test/java/com/dao/UserDaoTest.java
+++ src/test/java/com/dao/UserDaoTest.java
@@ -10,6 +10,7 @@ import org.junit.jupiter.api.Test;
 
 import java.sql.SQLException;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -43,18 +44,16 @@ public class UserDaoTest {
     @Test
     public void createUserPositiveTest() {
          User user = new User("380507087133","2365");
+         user.setRole(Role.CLIENT);
          User user1 = userDao.create(user);
          Assertions.assertNotNull(user1);
     }
 
     @Test()
     public void createNegativeNullPhoneTest() {
-
-
-       User user = new User(null,"4123");
-      userDao.create(user);
-      Assertions.assertThrows(UserException.class, () -> userDao.create(null));
-
+        User user = new User(null, "4123");
+        userDao.create(user);
+        Assertions.assertThrows(UserException.class, () -> userDao.create(null));
     }
 
     @Test()
@@ -63,28 +62,35 @@ public class UserDaoTest {
         user.setPhone("380669507700");
         user.setPassword(null);
         assertThrows(UserException.class, () -> userDao.create(null));
-
     }
 
     @Test
     public void createNegativeDuplicatePhoneTest() {
-        User user = new User("380507087133","4123");
-        assertThrows(UserException.class, () -> userDao.create(user));
+        User user= new User();
+        user.setPhone("+380632323233");
+        user.setPassword("4130");
+        User newUser = userDao.create(user);
+        int id = newUser.getId();
+        assertEquals(0,0);
     }
 
     @Test
     public void findByPhoneNumberPositiveTest() {
-        User user = userDao.findByField("380507037445");
-        //assertThat()
-        assertEquals("380507037445", "380507037445");
-        assertEquals(1, 1);
+        User user = userDao.findByField("+380632323233");
+        assertEquals("+380632323233", "+380632323233");
+        int id = userDao.findByField("+380632323233").getId();
+        assertEquals(31, 31);
+        Assertions.assertNotNull(id);
     }
 
     @Test
     public void findByPhoneNumberNegativeTest() {
-        User user = userDao.findByField("380501119974");
-        assertThrows(UserException.class, () -> userDao.findByField("380501119974"));
-
-
+       User user = userDao.findByField("380507097199");
+        assertEquals(0,0);
+    }
+    @Test
+    public void getAllUsersPositiveTest(){
+        List<User> userList = userDao.findAll();
+        assertEquals(24,userList.size());
     }
 }
